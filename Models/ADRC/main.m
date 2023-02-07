a3d516a14c5fd01b540574c5a7c13e5aff67dd62
Iris  David Du Mutel De Pierrepont F <iris.daviddu@polito.it>
2023-02-07 12:11:14 +0100

    Second order LADRC

--- Models/ADRC/main.m
+++ Models/ADRC/main.m
@@ -3,7 +3,7 @@
 %%% as a control tool for practitioners
 
 
-%%%% First order LADRC siso
+%% %% First order LADRC siso
 
 C = [1 0];
 A = [0 1; 0 0];
@@ -16,3 +16,22 @@ B = [b0; 0];
 Scl = -Kp;
 S_eso = 10*Scl;
 L = [-2*S_eso; S_eso^2];
+
+
+%% %% Second order LADRC siso
+
+C = [1 0 0];
+A = [0 1 0; 0 0 1; 0 0 0];
+K = 1;
+T = 1;
+D = 1;
+a = T^2;
+b = 2*D*T;
+b0 = K/T^2;
+Tsettle = 2;
+B = [0; b0; 0];
+Scl = -6/Tsettle;
+S_eso = 10*Scl;
+Kp = Scl^2;
+Kd = -2*Scl;
+L = [-3*S_eso; 3*S_eso^2; -S_eso^3];
